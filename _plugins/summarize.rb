d12fbf88b2343068f63b730f46c157fa706ef393
Sergi Mansilla <sergi.mansilla@gmail.com>
2013-11-12 16:43:47 +0100

    Get rid of summarize plugin

--- _plugins/summarize.rb
+++
@@ -1,9 +0,0 @@
-# Use like {{ post.content | summarize }} to only render everything before the <!--more--> comment (like word!@#ss)
-
-module Jekyll
-  module Filters
-    def summarize(str, splitstr = /<!--more-->/)
-      str.split(splitstr)[0]
-    end
-  end
-end
